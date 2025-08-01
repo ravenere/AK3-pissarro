3fba74abcc4710b07d9699eac012ea8dcce36d24
raven <161213198+ravenere@users.noreply.github.com>
2025-08-01 13:26:47 -0700

    update name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NOVA by Abdul7852
+kernel.string=elektrofrezirovshik by ravenere
 do.devicecheck=1
 do.modules=0
 do.systemless=1
