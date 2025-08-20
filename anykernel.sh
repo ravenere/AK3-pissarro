d1e94853f37404773a8b63fdb51cccc324120355
raven <161213198+ravenere@users.noreply.github.com>
2025-08-20 14:32:57 +0300

    mm

--- anykernel.sh
+++ anykernel.sh
@@ -1,50 +1,33 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
-kernel.string=elektrofrezirovshik by ravenere
-do.devicecheck=1
+kernel.string=sand kernel by @ravenere
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=pissarro
-device.name2=
-device.name3=
-device.name4=
-device.name5=
+device.name1=
 supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=1;
+# boot shell variables
+block=boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 no_block_display=1;
 
-
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 750 750 $ramdisk/*;
-
-
-## AnyKernel install
-dump_boot;
-
-# Begin Ramdisk Changes
-
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
-
-write_boot;
-## end install
+# boot install
+split_boot;
+flash_boot;
+flash_dtbo;
+## end boot install
